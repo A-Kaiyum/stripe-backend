6eb20690b40edf7e4ed138ac42771dd1b47b6f68
A. Kaiyum <akaiyum.cse@gmail.com>
2022-10-17 16:17:39 +0600

    stripe integrated in react with laravel

--- app/Http/Controllers/StripePaymentController.php
+++ app/Http/Controllers/StripePaymentController.php
@@ -10,27 +10,17 @@ use Stripe\Stripe;
 class StripePaymentController extends Controller
 {
 
-    public function makePayment()
+    public function makePayment(Request $request)
     {
 
         Stripe::setApiKey('sk_test_51LtSJLGiXzKYuOYkMt700dVTWeL5RG1a0e870EDiLRDuzgOkT7S0ylsMKUD2epCiLS5CvZD4imEFR7xDwuiWp7xZ00gQ3CCxeJ');
 
-        // function calculateOrderAmount(array $items): int {
-        //     // Replace this constant with a calculation of the order's amount
-        //     // Calculate the order total on the server to prevent
-        //     // people from directly manipulating the amount on the client
-        //     return 1400;
-        // }
-
         try {
-            // retrieve JSON from POST body
-            $jsonStr = file_get_contents('php://input');
-            $jsonObj = json_decode($jsonStr);
-
-            // Create a PaymentIntent with amount and currency
             $paymentIntent = PaymentIntent::create([
-                'amount' => 100.00 * 100.00,
+                'amount' =>  $request->amount,
                 'currency' => 'usd',
+                'description' => "This is test payment",
+                'receipt_email' => "srabon.tfp@gmail.com",
                 'automatic_payment_methods' => [
                     'enabled' => true,
                 ],
@@ -40,10 +30,10 @@ class StripePaymentController extends Controller
                 'clientSecret' => $paymentIntent->client_secret,
             ];
 
-            echo json_encode($output);
+            return response()->json($output);
         } catch (Error $e) {
-            http_response_code(500);
-            echo json_encode(['error' => $e->getMessage()]);
+
+            return response()->json(['error' => $e->getMessage()]);
         }
     }
 }
